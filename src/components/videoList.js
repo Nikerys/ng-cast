7a0261a994bd24c108bdde970302cdbb2dc49d2e
Hack Reactor Student <student@hackreactor.com>
2017-06-05 17:01:16 -0700

    connected templates to components

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,13 @@
 angular.module('video-player')
+// pass in the data from exampleVideoData
+// refactor the videoList component to dynamically 
+  //render one videoListEntry component for each video object in exampleVideoData
+
+// the exampleVideoData.js is our model (an ordinary JS obj)
+// we'll use the controller to build the logic for the video list view
+  // .controller('') scope glues the view with controller, 
+  // they hold the model data that we need to pass to view
+// binding, data binding
 .component('videoList', {
-  // TODO
+  templateUrl: 'src/templates/videoList.html'
 });
